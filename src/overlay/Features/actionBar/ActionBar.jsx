8b31c742bf26dab42f9d0b9ef0c41104b0ad2350
Nicky9319 <paarthsaxena2005@gmail.com>
2025-10-04 13:24:12 +0530

    Change button label from 'Action' to 'Add' in ActionBar component for clarity

--- src/overlay/Features/actionBar/ActionBar.jsx
+++ src/overlay/Features/actionBar/ActionBar.jsx
@@ -284,7 +284,7 @@ const ActionBar = () => {
               e.target.style.boxShadow = 'none';
             }}
           >
-            Action
+            Add
           </button>
         </div>
       </div>
